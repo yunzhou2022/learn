d03a7cc3a56222e0c9ba067f11e1293facd31df5
MacUser <903679675@qq.com>
2021-09-26 00:52:13 +0800

    🚀finish: 安装

---
+++ react/01官网重读、案例实现/01安装/02创建新的React应用/04_diy/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Temp from "./Temp";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> Hello, World! 22</h1>
+        <Temp />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react/01官网重读、案例实现/01安装/02创建新的React应用/04_diy/src/Temp.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+
+// class Temp extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1> Hello, World!TEMP11 </h1>
+//       </div>
+//     );
+//   }
+// }
+function Temp() {
+  return <h1>11</h1>;
+}
+
+export default hot(module)(Temp);
